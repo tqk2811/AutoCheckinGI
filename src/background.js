058ff848dbc2f432ff0c0ede8d0e8e6a432b3d65
tqk2811 <tqk2811@gmail.com>
2022-03-20 23:24:00 +0700

    update to manifest v3

--- src/background.js
+++ src/background.js
@@ -1,15 +1,10 @@
 ﻿'use strict';
-chrome.runtime.onStartup.addListener(function(){
-    setTimeout(onload,10000);
+chrome.runtime.onStartup.addListener(function () {
+    setTimeout(onload, 5000);
 });
-function onload(){
-    let request = new XMLHttpRequest();
-    request.open("POST","https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=vi-vn", true);
-    request.setRequestHeader("Content-Type","application/json;charset=UTF-8");
-    request.setRequestHeader("Accept","application/json, text/plain, */*");
-    request.setRequestHeader("Accept-Language", "vi,en;q=0.9,ja;q=0.8,ht;q=0.7");
-    request.onload = function(){
-        console.log("Status: " + this.status + ", Response: " + this.responseText);
-    }
-    request.send("{\"act_id\":\"e202102251931481\"}");
+async function onload() {
+    var res = await fetch(
+        "https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=vi-vn",
+        { method: "POST", body: JSON.stringify({ act_id: "e202102251931481" }) });
+    console.log(await res.text());
 }
