ad0ca78e52c7be4b212b1b3df50850406e1e208d
tqk2811 <tqk2811@gmail.com>
2022-05-23 20:47:26 +0700

    fix try again if request err

--- src/background.js
+++ src/background.js
@@ -3,8 +3,15 @@ chrome.runtime.onStartup.addListener(function () {
     setTimeout(onload, 5000);
 });
 async function onload() {
-    var res = await fetch(
-        "https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=vi-vn",
-        { method: "POST", body: JSON.stringify({ act_id: "e202102251931481" }) });
-    console.log(await res.text());
+    console.log("start");
+    try {
+        var res = await fetch(
+            "https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=vi-vn",
+            { method: "POST", body: JSON.stringify({ act_id: "e202102251931481" }) });
+        console.log(await res.text());
+    }
+    catch (e) {
+        console.log(e);
+        setTimeout(onload, 5000);
+    }
 }
