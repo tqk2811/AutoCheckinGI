bd95fb3c2a718b6cfdfd9cf33c6e5626dbe6a2d5
tqk2811 <tqk2811@gmail.com>
2023-01-18 09:30:14 +0700

    change url request to hoyolab.com

--- src/background.js
+++ src/background.js
@@ -3,12 +3,12 @@ chrome.runtime.onStartup.addListener(function () {
     setTimeout(onload, 5000);
 });
 async function onload() {
-    console.log("start");
     try {
-        var res = await fetch(
-            "https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=vi-vn",
+        let res = await fetch(
+            "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=vi-vn",
             { method: "POST", body: JSON.stringify({ act_id: "e202102251931481" }) });
-        console.log(await res.text());
+        let text = await res.text();
+        console.log(text);
     }
     catch (e) {
         console.log(e);
